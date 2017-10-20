126dde87de63e7d5590f25b7f9fb184b28f2b023
Juan Carlos <galamon5@hotmail.com>
2017-10-19 20:46:28 -0500

    Corregí el programa, no funciono la primera vez

--- Factorial.py
+++ Factorial.py
@@ -1,33 +1,40 @@
-def FDyV(n):
+def FDyV(n,div):
     if(len(n) < div):
         m = 1
         for i in range(len(n)):
-            m = m * int(n[i])#--------------
+            m = m * int(n[i])
         return m
     else:
-        divisiones = Dividir(n)
+        divisiones = Dividir(n,div)
         factorial = 1
         for j in range(div):
-            factorial = factorial * FDyV(divisiones[i])
+            factorial = factorial * FDyV(divisiones[j],div)
         return factorial
 
-def Dividir(n):
+def Dividir(n,div):
     k = 0
     divisiones = createArray(div)
     if(len(n)%div == 0):
         for i in range(div):
-            temp = ""
-            for j in range((len(n)/div)):
-                temp = temp + n[k++]#String.valueOf(n.charAt(k++))----------------
+            temp = []
+            for j in range(int(len(n)/div)):
+                temp.append(n[k])
+                k = k + 1
+            divisiones[i] = temp
+    else:
+        for i in range(div-1):
+            temp = []
+            for j in range(int(len(n)/div)):
+                temp.append(n[k])
+                k = k + 1
             divisiones[i] = temp
         divisiones[div-1] = n[k:]
     return divisiones
 
 def createArray(size):
     arrayAux = []
-    i = 0
     for i in range(size):
-        arrayAux.append(random.randint(0,size))
+        arrayAux.append('')
     return arrayAux
 
 def main():
@@ -35,6 +42,8 @@ def main():
     num = int(input("Ingresa el numero\n"))
     div = int(input("Ingresa numero de divisiones que deseas\n"))
     for i in range(num):
-        arreglo.append(i)
-    resultado = FDyV(arreglo)
-    print (num+"! = "resultado)
+        arreglo.append(str(i+1))
+    resultado = FDyV(arreglo,div)
+    print (str(num) + "! = " + str(resultado))
+
+main()
